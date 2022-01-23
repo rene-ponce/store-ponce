b648ee2988941e92e67c555abb5b50ddea9b7619
René Ponce <rene.ponce84@icloud.com>
2022-01-23 01:14:43 -0600

    Navbar and widget cart

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+import './ItemListContainer.css'
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <section className='item-list-container'>
+      <h2>{greeting}</h2>
+      <hr />
+    </section>
+  )
+}
