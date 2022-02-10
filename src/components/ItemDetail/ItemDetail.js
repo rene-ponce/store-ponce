38e66f0ddd549970f38b5160786a104aa0056ee4
René Ponce <rene.ponce@yaydoo.com>
2022-02-10 15:44:35 -0600

    Avances

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,31 @@
 export const ItemDetail = ({id, name, description, price, thumb, category}) => {
   return (
-    <div>
+    <div className="row">
       <h3>{name}</h3>
-      <img src={thumb} alt={name} />
-      <p>{description}</p>
-      <p>Precio ${price}</p>
+      <div className="col-md-6">
+        <img src={thumb} alt={name} />
+      </div>
+      <div className="col-md-6">
+        <p>{description}</p>
+        <h3>
+          <p>Precio: ${price}</p>
+        </h3>
+        <div className="input-group">
+          <span className="input-group-btn">
+            <button type="button" className="btn btn-danger btn-number"  data-type="minus" data-field="quant[2]">
+              <i class="bi bi-dash-lg"></i>
+            </button>
+          </span>
+          <input type="text" name="quant[2]" className="form-control input-number" min="1" max="100" />
+          <span class="input-group-btn">
+            <button type="button" className="btn btn-success btn-number" data-type="plus" data-field="quant[2]">
+              <i class="bi bi-plus-lg"></i>
+            </button>
+          </span>
+        </div>
+        <br />
+        <button className="btn btn-success btn-lg">Agregar al carrito</button>
+      </div>
     </div>
   )
 }
