1d9d7810a412af0599a18ac040bb6b9623b94822
Rene Ponce <renjes@Renes-Mac-mini.local>
2022-02-14 00:43:05 -0600

    Modify cart widget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
 
 export const CartWidget = () => {
+  const {cartQuantity} = useContext(CartContext)
   return (
-    <i className="bi bi-cart-fill"></i>
+    <Link to='/cart'>
+      <i className="bi bi-cart-fill"></i>
+      <span>{cartQuantity()}</span>
+    </Link>
   )
 }
